402da3df3219a16120b9884e03d0b86bc4b927a6
Thomas <9749173+uhthomas@users.noreply.github.com>
2020-07-25 23:58:51 +0100

    Use environment variables for workspace status var

--- hack/workspace-status.sh
+++ hack/workspace-status.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+if [ -z $GIT_COMMIT ]; then
+	GIT_COMMIT="$(git rev-parse head)"
+fi
+
 if [ -z $GIT_REF ]; then
 	GIT_REF="$(git tag --points-at head)"
 fi
 
-GIT_REF=${GIT_REF}
-
 cat <<EOF
-STABLE_GIT_COMMIT $(git rev-parse head)
+STABLE_GIT_COMMIT $GIT_COMMIT
 STABLE_GIT_REF $GIT_REF
 EOF
