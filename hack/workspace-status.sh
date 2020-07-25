bf8856c88f2b49817f75d7b8fec5711976adab31
Thomas <9749173+uhthomas@users.noreply.github.com>
2020-07-25 22:56:11 +0100

    Push individual images, as opposed to bundling

--- hack/workspace-status.sh
+++ hack/workspace-status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cat <<EOF
-GIT_COMMIT $(git rev-parse head)
-GIT_TAG $(git tag --points-at head)
+STABLE_GIT_COMMIT $(git rev-parse head)
+STABLE_GIT_TAG $(git tag --points-at head)
 EOF
--- images.bzl
+++
@@ -1,6 +0,0 @@
-def images(*tags):
-    images = {}
-    for tag in tags:
-        if tag:
-            images["docker.pkg.github.com/uhthomas/6f.io/root:" + tag] = ":image"
-    return images
