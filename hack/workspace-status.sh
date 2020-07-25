27b955cfc9bbcbbbcc1f4be5a0e2142a46eba17a
Thomas <9749173+uhthomas@users.noreply.github.com>
2020-07-25 23:31:14 +0100

    Remove astray artifact and pass ref

--- hack/workspace-status.sh
+++ hack/workspace-status.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+if [ -z $GIT_REF ]; then
+	GIT_REF="$(git tag --points-at head)"
+fi
+
 cat <<EOF
 STABLE_GIT_COMMIT $(git rev-parse head)
-STABLE_GIT_TAG $(git tag --points-at head)
+STABLE_GIT_REF $GIT_REF
 EOF
--- workspace-status.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cat <<EOF
-GIT_COMMIT $(git rev-parse head)
-GIT_TAG $(git tag --points-at head)
-EOF
